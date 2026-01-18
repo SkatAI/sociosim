a4b2fa511ad484527c6b0bc6d77a66b9ae75a670
Alexis Perrier <alexis.perrier@gmail.com>
2026-01-18 14:35:37 +0100

    Interviews => entretiens

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -144,7 +144,7 @@ export default function Header() {
                 color="fg.muted"
                 _hover={{ color: "accent.primary" }}
               >
-                {role === "admin" ? "Interviews" : "Mes entretiens"}
+                {role === "admin" ? "Entretiens" : "Mes entretiens"}
               </Link>
               {user_admin && (
                 <Link
